597ffdb94f91a3d7de3719acc11cc429d32ccf46
Hamza-benjelloun <hamza.benjelloun@octo.ma>
2023-04-16 09:46:30 +0000

    "Add data loading, encoding, train-test split, and model training and testing in main function."

--- collaborative-filtering/main.py
+++ collaborative-filtering/main.py
@@ -1,5 +1,6 @@
 import torch
 
+import pandas as pd
 from src.model.Model import RecSysModel
 from src.utils.MovieDataset import MovieDataset
 from src.utils.DataUtils import (
@@ -15,8 +16,8 @@ from src.utils.DataUtils import (
 import configparser
 import logging
 
-def main():
 
+def main():
     # set device
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -43,6 +44,51 @@ def main():
     optimizer = config["MODEL"]["OPTIMIZER"].lower()
     loss_function = config["MODEL"]["LOSS_FUNCTION"].lower()
     epochs = int(config["MODEL"]["EPOCHS"])
+
+    # load data
+    df = pd.read_csv(data_path)
+
+    # encode data
+    df = encode_data(df)
+
+    # get train test split
+    train_df, test_df = get_train_test_split(df)
+
+    # get data loaders
+    train_loader = get_data_loader(
+        MovieDataset, train_df, shuffle, batch_size, num_workers
+    )
+
+    test_loader = get_data_loader(
+        MovieDataset, test_df, shuffle, batch_size, num_workers
+    )
+
+    # get model
+    model = RecSysModel(df["userId"].max() + 1, df["movieId"].max() + 1).to(device)
+
+    # get optimizer
+    optimizer = get_optimizer(optimizer, model)
+
+    # get loss function
+    loss_function = get_loss_function(loss_function)
+
+    # get scheduler
+    scheduler = get_scheduler(optimizer, step_size, gamma)
+
+    # train
+    train(
+        model,
+        train_loader,
+        test_loader,
+        optimizer,
+        loss_function,
+        scheduler,
+        epochs,
+        device,
+    )
+
+    # test
+    test(model, test_loader, loss_function, device)
 
 
 if __name__ == "__main__":
